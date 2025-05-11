3f99e8946da8a3b4db3f6a2245646a0a8b50ec04
Alejandro Tapia <78757063+AlejandroSTapia@users.noreply.github.com>
2025-05-11 13:44:31 -0600

    Add api crud
    Se agrego los endpoints para la api de notas
    CRUD

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import app from "./app";
-import { getConnection } from "./database/connection";
+import dotenv from "dotenv";
+import path from "path";
+
+const env = process.env.NODE_ENV || 'dev';
+dotenv.config({ path: path.resolve(__dirname, `.env.${env}`) });
 
 app.listen(3000);
 console.log('Servidor iniciado en port 3000');
