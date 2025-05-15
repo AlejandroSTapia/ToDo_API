556ade639e8ee6f7f24be99ceb880b10f1b65a91
Alejandro Tapia <78757063+AlejandroSTapia@users.noreply.github.com>
2025-05-14 22:48:55 -0600

    Fix build
    Se arreeglo tsconfig y se ejceuto build y se obtuvo dist

---
+++ dist/database/connection.js
@@ -0,0 +1,28 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getConnection = void 0;
+const mssql_1 = __importDefault(require("mssql"));
+const dbSettings = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
+    options: {
+        encrypt: process.env.DB_ENCRYPT === 'true',
+        trustServerCertificate: process.env.DB_TRUST_CERT === 'true',
+    }
+};
+const getConnection = async () => {
+    try {
+        const pool = await mssql_1.default.connect(dbSettings);
+        return pool;
+    }
+    catch (error) {
+        console.error('Database connection error:', error);
+        throw error;
+    }
+};
+exports.getConnection = getConnection;
